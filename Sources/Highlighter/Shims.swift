c15eea4e5e8f18b0a2d2173830fcaa250f742bc0
Alfian Losari <alfianlosari@gmail.com>
2023-04-09 18:09:23 +0700

    Shims - Fix build error on tvOS/watchOS

--- Sources/Highlighter/Shims.swift
+++ Sources/Highlighter/Shims.swift
@@ -11,25 +11,19 @@ import Foundation
 
 #if os(OSX)
     import AppKit
-#elseif os(iOS)
+#else
     import UIKit
 #endif
 
 /**
  Set type aliases according to which Swift is being run and,
  in the second case, if we're running on iOS
-
- NOTE This is probably unnecessary now since the Swift Package
-      mandates Swift 5.3
  */
-#if swift(>=4.2)
-    public typealias AttributedStringKey = NSAttributedString.Key
-#else
-    public typealias AttributedStringKey = NSAttributedStringKey
-#endif
 
-#if swift(>=4.2) && os(iOS)
-    public typealias TextStorageEditActions = NSTextStorage.EditActions
-#else
+public typealias AttributedStringKey = NSAttributedString.Key
+
+#if os(macOS)
     public typealias TextStorageEditActions = NSTextStorageEditActions
+#else
+    public typealias TextStorageEditActions = NSTextStorage.EditActions
 #endif
